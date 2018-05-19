3527e69e59f0b5a25f77ce1b902f5a2f262d8837
Utkarsh Nadkarni <utkarsh.nadkarni@gmail.com>
2018-05-18 22:54:42 -0400

    first commit

---
+++ omaha-service/src/main/java/com/example/omahaservice/OmahaServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.omahaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmahaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmahaServiceApplication.class, args);
+	}
+}
